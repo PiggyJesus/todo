0bd6cfaa1c96b78b6720a43532fdb22ae09d05c1
PiggyJesus <Kulakov.Stanislav82@gmail.com>
2023-07-17 13:19:56 +0300

    CI via github actions

--- lib/core/utils/my_strings.dart
+++ lib/core/utils/my_strings.dart
@@ -1,5 +1,5 @@
 abstract class MyStrings {
   static const String baseUrl = "https://beta.mrdekk.ru/todobackend";
   static const String revision = "revision";
-  static const String bearerKey = ""; // TODO вставить свой токен
+  static const String bearerKey = "crankled"; // можно поменять на свой
 }
